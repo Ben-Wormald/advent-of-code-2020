8e926b4a8d33eb1ae01cdadda2d36b633b7ec3b6
Ben-Wormald <benwormald95@gmail.com>
2020-12-01 20:43:56 +0000

    tidy up

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+#![allow(dead_code)]
 use std::fs;
 
-mod solutions {
-    pub mod day_01;
-}
+mod solutions { pub mod day_01; }
+use solutions::day_01::solve;
+const INPUT: &str = "./input/01";
 
 fn main() {
-    let input = fs::read_to_string("./input/01")
-        .expect("oh no");
-    let result = solutions::day_01::solve(&input);
+    let input = fs::read_to_string(INPUT).expect("oh no!");
+    let result = solve(&input);
     println!("{}", result)
 }
