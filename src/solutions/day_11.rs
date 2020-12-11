021e680dce8123c47b95c0d719c5926aaf47b374
Ben-Wormald <benwormald95@gmail.com>
2020-12-11 11:46:03 +0000

    solve day 11 pt 2

--- src/solutions/day_11.rs
+++ src/solutions/day_11.rs
@@ -5,7 +5,7 @@ enum Cell {
     Occupied,
 }
 
-pub fn solve(input: &str) -> usize {
+pub fn solve_part_one(input: &str) -> usize {
     let mut grid = process(input);
     let mut is_stable = false;
 
@@ -79,4 +79,78 @@ fn count_occupied(grid: &Vec<Vec<Cell>>) -> usize {
                     }
                 })
         })
+}
+
+pub fn solve(input: &str) -> usize {
+    let mut grid = process(input);
+    let mut is_stable = false;
+
+    while !is_stable {
+        let mut grid_new = grid.clone();
+        is_stable = true;
+
+        for y in 0..grid.len() {
+            for x in 0.. grid[y].len() {
+                grid_new[y][x] = new_state_sightlines(&grid, x, y);
+
+                if grid_new[y][x] != grid[y][x] {
+                    is_stable = false;
+                }
+            }
+        }
+        grid = grid_new;
+    }
+
+    count_occupied(&grid)
+}
+
+fn new_state_sightlines(grid: &Vec<Vec<Cell>>, x: usize, y: usize) -> Cell {
+    let mut occupied = 0;
+
+    for dy in -1..2 as isize {
+        for dx in -1..2 as isize {
+            if !(dy == 0 && dx == 0) {
+
+                let mut found = false;
+                let mut i: isize = 1;
+                while !found {
+                    let search_y = (y as isize) + i * dy;
+                    let search_x = (x as isize) + i * dx;
+
+                    if search_y < 0 || search_y >= grid.len() as isize || search_x < 0 || search_x >= grid[0].len() as isize {
+                        found = true;
+                    }
+                    else if grid[search_y as usize][search_x as usize] == Cell::Empty {
+                        found = true;
+                    }
+                    else if grid[search_y as usize][search_x as usize] == Cell::Occupied {
+                        occupied += 1;
+                        found = true;
+                    }
+
+                    i += 1;
+                }
+            }
+        }
+    }
+
+    match grid[y][x] {
+        Cell::Empty => if occupied == 0 { Cell::Occupied } else { grid[y][x] },
+        Cell::Occupied => if occupied >= 5 { Cell::Empty } else { grid[y][x] },
+        _ => grid[y][x],
+    }
+}
+
+fn print_grid(grid: &Vec<Vec<Cell>>) -> () {
+    for row in grid {
+        for cell in row {
+            print!("{}", match cell {
+                Cell::Floor => ".",
+                Cell::Empty => "L",
+                Cell::Occupied => "#",
+            });
+        }
+        print!("\n");
+    }
+    print!("\n");
 }
