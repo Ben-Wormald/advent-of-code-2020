c4a103edd25bd8a63a9e5385b5584ff46470fe78
Ben-Wormald <benwormald95@gmail.com>
2020-12-19 16:53:53 +0000

    solve day 19 pt 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![allow(dead_code)]
 use std::fs;
 
-mod solutions { pub mod day_18; }
-use solutions::day_18::solve;
-const INPUT: &str = "./input/18";
+mod solutions { pub mod day_19; }
+use solutions::day_19::solve;
+const INPUT: &str = "./input/19";
 
 fn main() {
     let input = fs::read_to_string(INPUT).expect("oh no!");
---
+++ src/solutions/day_19.rs
@@ -0,0 +1,111 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+enum Rule {
+    Terminal(char),
+    Nonterminal(Vec<Vec<usize>>),
+}
+
+pub fn solve(input: &str) -> usize {
+    let (rules, words) = process(input);
+
+    let initial_rule = match rules.get(&0).unwrap() {
+        Rule::Nonterminal(options) => &options[0],
+        Rule::Terminal(_) => panic!("hmm"),
+    };
+
+    words
+        .iter()
+        .fold(0, |sum, word| match validate_word(word, &initial_rule, &rules) {
+            Ok(consumed) => if consumed == word.len() { sum + 1 } else { sum },
+            Err(()) => sum,
+        })
+}
+
+fn process(input: &str) -> (HashMap<usize, Rule>, Vec<&str>) {
+    let parts: Vec<&str> = input.split("\n\n").collect();
+
+    let mut rules: HashMap<usize, Rule> = HashMap::new();
+
+    parts[0]
+        .lines()
+        .for_each(|line| {
+            let line_parts: Vec<&str> = line.split(": ").collect();
+            let rule_id = line_parts[0].parse().unwrap();
+
+            let rule = if line_parts[1].starts_with("\"") {
+                Rule::Terminal(
+                    line_parts[1].replace("\"", "").chars().next().unwrap()
+                )
+            }
+            else {
+                let rule_options = line_parts[1]
+                    .split(" | ")
+                    .map(|rule_option| rule_option
+                        .split_whitespace()
+                        .map(|rule_part| rule_part.parse().unwrap())
+                        .collect()
+                    )
+                    .collect();
+
+                Rule::Nonterminal(rule_options)
+            };
+
+            rules.insert(rule_id, rule);
+        });
+
+    let words = parts[1].lines().collect();
+
+    (rules, words)
+}
+
+fn validate_word(word: &str, rule_ids: &[usize], rules: &HashMap<usize, Rule>) -> Result<usize, ()> {
+    if word.len() == 0 && rule_ids.len() == 0 {
+        return Ok(0);
+    }
+    else if (word.len() > 0 && rule_ids.len() == 0) || (word.len() == 0 && rule_ids.len() > 0) {
+        return Err(());
+    }
+
+    let mut consumed = 0;
+
+    for i in 0..rule_ids.len() {
+        let rule = rules.get(&rule_ids[i]).unwrap();
+        let remaining = &word[consumed..];
+        
+        match rule {
+            Rule::Terminal(t) => {
+                if remaining.chars().next().unwrap() == *t {
+                    consumed += 1;
+                }
+                else {
+                    return Err(());
+                }
+            },
+            Rule::Nonterminal(options) => {
+                let mut options_valid = false;
+                let mut options_consumed = 0;
+
+                for option in options {
+                    match validate_word(remaining, option, rules) {
+                        Ok(option_consumed) => {
+                            options_valid = true;
+                            options_consumed = option_consumed;
+                            break;
+                        },
+                        Err(()) => ()
+                    }
+                }
+
+                if options_valid {
+                    consumed += options_consumed;
+                }
+                else {
+                    return Err(());
+                }
+            }
+        }
+    }
+
+    Ok(consumed)
+}
